2e044a56e9768ed24b659f2cfb1463fe743fd767
SID <siddheshlamture9@gmail.com>
2026-09-12 11:25:35 +0530

    Practiced template function and template class

--- OOP codes/Exception handling.cpp
+++ OOP codes/Exception handling.cpp
@@ -15,14 +15,48 @@ class calculator{
 
     }
 };
-int main(){
-    calculator<int> c1;
-    c1.getdata();
-    c1.add();
-    
-    calculator<float> c2;
-    c2.getdata();
-    c2.add();
 
+
+// int main(){
+//     calculator<int> c1;
+//     c1.getdata();
+//     c1.add();
+    
+//     calculator<float> c2;
+//     c2.getdata();
+//     c2.add();
+
+//     return 0;
+// }
+
+// 
+
+class error{
+    public:
+    void message(){
+        cout<<"General error";
+
+    }
+};
+class divide:public error{
+    public:
+    void display(){
+        cout<<"Cannot divide by zero";
+
+    }
+};
+int main(){
+    int a=10;
+    int b=5;
+
+    try{
+        if(b==0){
+            throw divide();
+        }
+        cout<<a/b;
+    }
+    catch(divide e){
+        e.display();
+    }
     return 0;
 }
